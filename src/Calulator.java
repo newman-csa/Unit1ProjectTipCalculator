8cc81b9f8bce3c003fc127b5d7600d597f403799
newman-csa <logann24@nycstudents.net>
2023-09-27 09:34:46 -0400

    Updated formatting

--- src/Calulator.java
+++ src/Calulator.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Calulator {
+import  java.util.Scanner;
+
+public class Calulator
+{
+    public static void mai(String[] arg)
+    {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("--This is a tip calculator--\n");
+        System.out.print("What is your total bill?: ");
+        int totalBill = scn.nextInt();
+        System.out.print("\nHow many people are splitting the tip?: ");
+        int numberPeople = scn.nextInt();
+        System.out.print("\nHow many people are splitting the tip?: ");
+        int tipPercentage = scn.nextInt();
+
+
+    }
 }
